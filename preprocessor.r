fc4b1abb0628126b6dee05ad6021ec8b37867ea4
episode <dlrlgns58@gmail.com>
2020-11-12 21:03:30 +0900

    fix bugs and errors

--- preprocessor.r
+++ preprocessor.r
@@ -3,9 +3,10 @@ library(dplyr)
 library(tm)
 library(SnowballC)
 
+
 #reading companies csv file - beware of encoding
 csv1 = read.csv("all_review.csv", encoding = "UTF-8")
-csv1_name = "all"
+csv1_name = "all_sparse"
 
 
 ################################
@@ -14,30 +15,30 @@ csv1_name = "all"
 
 preprocessor = function(csv, name){
   
-  #drop NA and trimming records to 400
-  if (nrow(csv) > 400){
-    csv = csv[1:400,]; nrow(csv)
-  }
-  
   #pros & cons columns 
   pros = csv$pros
   cons = csv$cons
+
   
   #translate text vector data to corpus
-  text_corpus_pros = Corpus(VectorSource(pros))
-  text_corpus_cons = Corpus(VectorSource(cons))
+
   
   #all text to lowercase
   #removing numbers, puncuations, stopwords
+  text_corpus_pros <- iconv(pros, "ASCII", "UTF-8", sub="byte")
+  text_corpus_pros = Corpus(VectorSource(text_corpus_pros))
   corpus_pros = tm_map(text_corpus_pros, content_transformer(tolower))
   corpus_pros = tm_map(corpus_pros, removeNumbers)
-  corpus_pros = tm_map(corpus_pros, removePunctuation)
+  corpus_pros = tm_map(corpus_pros, removePunctuation)  
   corpus_pros = tm_map(corpus_pros, removeWords, stopwords())
-  
+
+  text_corpus_cons <- iconv(cons, "ASCII", "UTF-8", sub="byte")
+  text_corpus_cons = Corpus(VectorSource(text_corpus_cons))
   corpus_cons = tm_map(text_corpus_cons, content_transformer(tolower))
   corpus_cons = tm_map(corpus_cons, removeNumbers)
   corpus_cons = tm_map(corpus_cons, removePunctuation)
   corpus_cons = tm_map(corpus_cons, removeWords, stopwords())
+
   
   #stemming by stemDocument from snowballC
   corpus_pros = tm_map(corpus_pros, stemDocument)
@@ -54,10 +55,11 @@ preprocessor = function(csv, name){
   
   #removing sparsity
   tdm_pros = removeSparseTerms(tdm_pros, sparse = 0.98)
-  mat_pros = as.matrix(tdm_pros); mat_pros
+  mat_pros = as.matrix(tdm_pros)
+
   
   tdm_cons = removeSparseTerms(tdm_cons, sparse = 0.98)
-  mat_cons = as.matrix(tdm_cons); mat_cons
+  mat_cons = as.matrix(tdm_cons)
   
   #excel column merging
   
